1a6c790caa3a600674571d0bf8add1c41556453f
jwsnod <111009619+jwsnod@users.noreply.github.com>
2022-08-12 14:34:44 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -23,8 +23,8 @@ streamlit.dataframe(fruits_to_show)
 #create new function
 def get_fruityvice_data(this_fruit_choice):
    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+this_fruit_choice)
-    fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-    return fruityvice_normalized
+   fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+   return fruityvice_normalized
 #New section to display Fruity Vice response
 streamlit.header('Fruityvice Fruit Advice!')
 try:
@@ -35,7 +35,7 @@ try:
     back_from_function = get_fruityvice_data(fruit_choice)
     streamlit.dataframe(back_from_function)
 except URLError as e:
-   streamlit.error()
+  streamlit.error()
 
 
 streamlit.stop()
